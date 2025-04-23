b0ed9e4c7d4357abdc16f2a1da7a90b5ee4fe680
Georgy Sabaev <gasabaev@edu.hse.ru>
2025-04-24 01:46:23 +0300

    add textmate bundle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ intellij {
     version.set("2024.1.7")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf("org.jetbrains.plugins.textmate"))
 }
 
 tasks {
