f54fdafb152bdc0f1a2e5f493a1eafa4ab6561ec
Georgy Sabaev <gasabaev@edu.hse.ru>
2025-04-25 00:32:54 +0300

    os detection

--- src/main/java/eointellijlspplugin/EoPreloadingActivity.java
+++ src/main/java/eointellijlspplugin/EoPreloadingActivity.java
@@ -4,19 +4,29 @@ import com.intellij.openapi.application.PreloadingActivity;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.progress.ProgressIndicator;
 import org.wso2.lsp4intellij.IntellijLanguageClient;
-import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition;
 import com.intellij.ide.plugins.PluginManagerCore;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import org.apache.commons.lang3.SystemUtils;
 
 public class EoPreloadingActivity extends PreloadingActivity {
+
+    private String getBinaryName(){
+        if (SystemUtils.IS_OS_WINDOWS) {
+            return "server-win.exe";
+        }
+        if (SystemUtils.IS_OS_LINUX) {
+            return "server-linux";
+        }
+        if (SystemUtils.IS_OS_MAC_OSX) {
+            return "server-macos";
+        }
+        throw new RuntimeException("OS not supported.");
+    }
 
     @Override
     public void preload() {
         var pluginPath = PluginManagerCore.getPlugin(PluginId.getId("com.example.eo-intellij-lsp-plugin")).getPluginPath();
-        var serverPath = pluginPath.resolve("server-linux");
+        var serverPath = pluginPath.resolve(getBinaryName());
         IntellijLanguageClient.addServerDefinition(new EoServerDefinition("eo", new String[]{serverPath.toString(), "--stdio"}));
     }
 
